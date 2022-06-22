fcd4f19561420006afd0b77672fdf7e0e5b3a7e2
Gabriel Batista <Gabrielbatista78@hotmail.com>
2022-06-22 19:30:16 -0300

    Criando validação + e métodos do CRUD para o cliente

---
+++ src/main/java/br/com/sale/dto/ClientDTO.java
@@ -0,0 +1,65 @@
+package br.com.sale.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.sale.domain.Client;
+
+public class ClientDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	@NotEmpty(message = "Campo obrigatório")
+	@Length(min = 5, max = 80, message = "Você deve preencher esse campo de 5 à 80 caracteres")
+	private String nome;
+	
+	@Email
+	private String email;
+
+	public ClientDTO() {
+	}
+	
+	public ClientDTO(Client entity) {
+		id = entity.getId();
+		nome = entity.getName();
+		email = entity.getEmail();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+		
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public static Client fromDTO(ClientDTO dto) {
+		return new Client(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+	
+	
+	
+	
+}
--- src/main/java/br/com/sale/resource/ClientResource.java
+++ src/main/java/br/com/sale/resource/ClientResource.java
@@ -1,15 +1,22 @@
 package br.com.sale.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sale.domain.Client;
+import br.com.sale.dto.ClientDTO;
 import br.com.sale.service.ClientService;
 
 @RestController
@@ -18,17 +25,43 @@ public class ClientResource {
 
 	@Autowired
 	private ClientService catService;
-
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = catService.findAll();
-		return ResponseEntity.ok().body(list);
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClientDTO entityDTO, @PathVariable Long id) {
+		catService.update(ClientDTO.fromDTO(entityDTO));
+		return ResponseEntity.noContent().build();
 	}
-
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+			catService.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Client> findById(@PathVariable Long id) {
 		Client obj = catService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<Client> list = catService.findAll();
+		List<ClientDTO> listDTO = list.stream().map(obj -> new ClientDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClientDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0")  Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+		Page<Client> list = catService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClientDTO> listDTO = list.map(obj -> new ClientDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 
 }
